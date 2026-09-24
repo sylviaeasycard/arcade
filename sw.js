a3b88077f86ab4ce099c1d81f4ef6150d1252252
sylviaeasycard <sylvia.easycard@gmail.com>
2026-09-24 21:56:25 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,39 @@
+const CACHE_NAME = 'arcade-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+// 安裝 Service Worker 並快取資源
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('milk-v3').then((cache) => cache.addAll(['./index.html', './manifest.json']))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
 });
 
+// 攔截請求：優先使用快取，無快取才發送網路請求
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
+
+// 清理舊快取
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
   );
 });
